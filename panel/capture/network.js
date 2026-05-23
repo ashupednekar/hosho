2ee804eb4dd1f1b897a1898b1212805f258553f1
ashupednekar <ashutoshpednekar15@gmail.com>
2026-05-23 18:40:35 +0530

    Keep redaction in capture package

--- panel/capture/network.js
+++ panel/capture/network.js
@@ -1,17 +1,6 @@
 import { ENDPOINTS } from "../config.js";
 import { recordTrace } from "../state.js";
-
-const SENSITIVE_NAMES = new Set([
-  "authorization",
-  "cookie",
-  "set-cookie",
-  "x-api-key",
-  "token",
-  "access_token",
-  "refresh_token",
-  "secret",
-  "key",
-]);
+import { redactNetworkEntry } from "./redaction.js";
 
 export function startNetworkCapture({ chromeDevtools, state, transport, render }) {
   chromeDevtools.network.onRequestFinished.addListener((entry) => {
@@ -32,38 +21,4 @@ export function startNetworkCapture({ chromeDevtools, state, transport, render }
       ms: Number(entry.time) || 0,
     });
   });
-}
-
-function redactNetworkEntry(entry) {
-  const copy = JSON.parse(JSON.stringify(entry));
-
-  redactHeaders(copy.request?.headers);
-  redactHeaders(copy.response?.headers);
-  redactQueryString(copy.request?.queryString);
-
-  if (copy.request?.postData?.text) {
-    copy.request.postData.text = "[redacted]";
-  }
-
-  return copy;
-}
-
-function redactHeaders(headers = []) {
-  for (const header of headers) {
-    if (isSensitiveName(header.name)) {
-      header.value = "[redacted]";
-    }
-  }
-}
-
-function redactQueryString(params = []) {
-  for (const param of params) {
-    if (isSensitiveName(param.name)) {
-      param.value = "[redacted]";
-    }
-  }
-}
-
-function isSensitiveName(name = "") {
-  return SENSITIVE_NAMES.has(name.toLowerCase());
 }
